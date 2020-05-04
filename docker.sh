2cef3a42cc4ed68ed8e28e63a7c80d2b9945cf17
Łukasz Kałużny <lukasz@kaluzny.pro>
2020-05-04 08:38:05 +0200

    Fix Docker

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f jekyll
-docker run --rm -it -p 4000:4000 -v $(PWD):/srv/jekyll --name pato jekyll/jekyll:3.8.5 jekyll serve --drafts --watch
+docker run --rm -it -p 4000:4000 -v $(PWD):/srv/jekyll --name jekyll jekyll/jekyll:3.8.5 jekyll serve --drafts --watch
